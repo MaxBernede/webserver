6307d1987b1539f9e88ca6804f8a9064b7fbad2a
Yesim Zaim <yzaim@f1r5s18.codam.nl>
2024-04-11 19:13:38 +0200

    will merge socket class to the code

--- src/main.cpp
+++ src/main.cpp
@@ -60,6 +60,12 @@ Server	pushBlock(std::list<std::string> block, char **env){
 	return serv;
 }
 
+void serverLoop()
+{
+
+}
+
+// TODO add signal handler to close all fd's when someone presses CTRL C
 int main(int argc, char** argv, char** env) {
 
 	//!KOEN WORK 
@@ -82,53 +88,55 @@ int main(int argc, char** argv, char** env) {
 	// }
 
 
-    //!START OF MY BASE WORK
-    // Create a socket
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        std::cerr << "Error creating socket" << std::endl;
-        return 1;
-    }
+	//!START OF MY BASE WORK
+	// Create a socket
+	Socket s(server.port);
 
-    // Prepare the server address structure
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1) {
+		std::cerr << "Error creating socket" << std::endl;
+		return 1;
+	}
+
+	// Prepare the server address structure
+	struct sockaddr_in server_addr;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
 	int p = genererPort();
-    std::cout << "http://localhost:" << p << std::endl;
-    server_addr.sin_port = htons(p);
+	std::cout << "http://localhost:" << p << std::endl;
+	server_addr.sin_port = htons(p);
 
-    // Bind the socket to the specified IP and port
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        std::cerr << "Bind failed" << std::endl;
-        return 1;
-    }
+	// Bind the socket to the specified IP and port
+	if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+		std::cerr << "Bind failed" << std::endl;
+		return 1;
+	}
 
-    // Listen for incoming connections
-    if (listen(server_fd, 10) == -1) {
-        std::cerr << "Listen failed" << std::endl;
-        return 1;
-    }
+	// Listen for incoming connections
+	if (listen(server_fd, 10) == -1) {
+		std::cerr << "Listen failed" << std::endl;
+		return 1;
+	}
 
-    while (true) {
-        // Accept a new connection
-        int client_fd = accept(server_fd, nullptr, nullptr);
-        if (client_fd == -1) {
-            std::cerr << "Accept failed" << std::endl;
-            continue;
-        }
+	while (true) {
+		// Accept a new connection
+		int client_fd = accept(server_fd, nullptr, nullptr);
+		if (client_fd == -1) {
+			std::cerr << "Accept failed" << std::endl;
+			continue;
+		}
 
-        Response response(client_fd);
-        std::cout << "Client connected: " << client_fd << std::endl;
+		Response response(client_fd);
+		std::cout << "Client connected: " << client_fd << std::endl;
 		std::cout << "Sleeping for 10 seconds..." << std::endl;
 		std::this_thread::sleep_for(std::chrono::seconds(10));
 		std::cout << "Awake after 10 seconds!" << std::endl;
-        // Handle the request from the client
-        response.handle_request();
+		// Handle the request from the client
+		response.handle_request();
 
-        close(client_fd);
-    }
+		close(client_fd);
+	}
 
-    close(server_fd);
-    return 0;
+	close(server_fd);
+	return 0;
 }
