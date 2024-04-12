11869de8333886d3a1012d197274eb1110a7dbb5
Koen Posthuma <kposthum@f1r5s18.codam.nl>
2024-04-12 14:17:40 +0200

    working out post requests?

--- Request.cpp
+++ Request.cpp
@@ -1,10 +1,26 @@
-#include"Request.hpp"
 #include<unistd.h>
 #include<fcntl.h>
+#include<list>
+#include<iostream>
+#include"Request.hpp"
 
 Request::Request(){}
 
+static std::list<std::string> makeRequest(std::string str){
+	std::list<std::string> request;
+	std::string temp;
+	int i = 0;
+	while (str != "" && i < 30){
+		temp = str.substr(0, str.find('\n'));
+		request.push_back(temp);
+		str.erase(0, temp.length() + 1);
+		i++;
+	}
+	return request;
+}
+
 Request::Request(int fd){
+
 	char buffer[1024];
 	std::string request;
 	int i = 0;
@@ -12,29 +28,33 @@ Request::Request(int fd){
 		buffer[i] = '\0';
 		request += buffer;
     }
-	_request = request;
+	_request = makeRequest(request);
 	size_t j = request.find_first_of(" ");
 	std::string temp = request.substr(0, j);
 	for (int k = 0; k <= TRACE; k++){
 		if (temp == methodType[k])
-			_type = k;
+			_method = k;
 	}
 	size_t l = request.find("Host: ") + 6;
 	_host = request.substr(l, request.find("\n", l) - l);
-	size_t n = request.find("Content-Length: ");
-	if (n != std::string::npos){
-		temp = request.substr((n + 16), request.find("\n", n) - n);
+	if (_method == POST){
+		l = request.find("Content-Type: ");
+		_contentType = request.substr((l + 14), request.find("\n", l) - (l + 14));
+		l = request.find("Content-Length: ");
+		temp = request.substr((l + 16), request.find("\n", l) - (l + 16));
 		_size = std::stol(temp);
+		temp = _request.back();
+		_content = temp.substr(temp.find('=') + 1, temp.length());
 	}
 }
 
 Request::Request(std::string request){
-	_request = request;
+	_request = makeRequest(request);
 	size_t j = request.find_first_of(" ");
 	std::string temp = request.substr(0, j);
 	for (int k = 0; k < TRACE; k++){
 		if (temp == methodType[k])
-			_type = k;
+			_method = k;
 	}
 	size_t l = request.find("Host: ") + 6;
 	size_t m = request.find("\n", l);
@@ -42,17 +62,34 @@ Request::Request(std::string request){
 }
 
 int	Request::getType() const{
-	return _type;
+	return _method;
 }
 
 std::string	Request::getHost() const{
 	return _host;
 }
 
+std::string	Request::getContType() const{
+	return _contentType;
+}
+
+uint32_t	Request::getSize() const{
+	return _size;
+}
+
+std::string	Request::getContent() const{
+	return _content;
+}
+
 std::ostream & operator<< (std::ostream &out, const Request& src){
 	out << "Request info:\n"
-		<< "type:\t" << src.getType() << "\n"
+		<< "type:\t" << src.getType() << '\n'
 		<< "host:\t" << src.getHost() << std::endl;
+	if (src.getType() != GET){
+		out << "content type:\t" << src.getContType() << '\n'
+			<< "size:\t" << src.getSize() << '\n'
+			<< "content:\t" << src.getContent() << std::endl;
+	}
 	return out;
 }
 
--- Request.hpp
+++ Request.hpp
@@ -1,7 +1,18 @@
 #pragma once
 
 #include <string>
-#include "Server.hpp"
+#include<list>
+// #include "Server.hpp"
+enum methods{
+	GET,
+	POST,
+	DELETE,
+	PUT,
+	PATCH,
+	CONNECT,
+	OPTIONS,
+	TRACE
+};
 
 const static std::string methodType[8] = {
 	"GET",
@@ -21,11 +32,16 @@ class Request {
 
 		int			getType() const;
 		std::string	getHost() const;
+		std::string	getContType() const;
+		uint32_t	getSize() const;
+		std::string	getContent() const;
 	private:
-		std::string _request;
-		int			_type;
-		std::string	_host;
-		uint32_t	_size;
+		std::list<std::string>	_request; //the entirety of the request, as a list of strings
+		int						_method;
+		std::string				_host;
+		std::string				_contentType; // what content are we trying to POST?
+		uint32_t				_size;	//for POST requests, cannot be higher then max body
+		std::string				_content; // the content of the POST request string? maybe something else?
 };
 
 std::ostream & operator<< (std::ostream &out, const Request& src);
