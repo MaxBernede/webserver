7e5817ed83621f36c886b9cbad19a375f7a4f641
yesimzaim <yesimzaim@gmail.com>
2024-04-25 15:18:09 +0200

    working on cgi on this branch

--- inc/CGI.hpp
+++ inc/CGI.hpp
@@ -4,6 +4,9 @@
 
 class CGI {
 	private:
+		int _receivePipe[2]; // pipe where CGI receives data
+		int _sendPipe[2]; // pipe where CGI sends data
+		std::string _response;
 
 	public:
 		CGI(void);
