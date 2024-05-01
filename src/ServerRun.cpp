46370a5ffb40c02e7276c51f0de291e51311a063
Yesim Zaim <yzaim@f1r6s18.codam.nl>
2024-05-01 17:50:53 +0200

    writing notes

--- src/ServerRun.cpp
+++ src/ServerRun.cpp
@@ -243,3 +243,17 @@ void ServerRun::dataOut(s_poll_data pollData, struct pollfd pollFd, int idx)
 			break ;
 	}
 }
+
+/*
+
+vector requests
+vector responses (response string)
+vector cgi
+
+CLIENT_CONNECTION (write here) --> read to see the request / write to response
+CGI_PIPE_READ (read html file) --> to create the response string
+FILE_READ (read html file) --> to create the response string
+FILE_WRITE (needed for post method?)
+LISTENER_SOCKET (read to see new connection requests)
+
+*/
